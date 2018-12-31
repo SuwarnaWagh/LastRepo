6c4a6c82e5559eafdaa72cfe5dca15466724a18e
Admin <su.wagh30@gmail.com>
2019-01-01 00:34:25 +0530

    updated TestCase1

--- src/test/java/webUITestCases/TestCase1_Button.java
+++ src/test/java/webUITestCases/TestCase1_Button.java
@@ -1,5 +1,12 @@
 package webUITestCases;
 
+import org.testng.annotations.Test;
+
 public class TestCase1_Button {
 
+	
+	@Test
+	public void welcome() {
+		System.out.println("Hi, You are in git repository project :)");
+	}
 }
