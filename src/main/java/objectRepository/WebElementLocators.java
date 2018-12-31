dbc897f8a9209584991046f9f132846309768e26
Admin <su.wagh30@gmail.com>
2019-01-01 00:23:28 +0530

    updated POM.xml & OR class file

--- src/main/java/objectRepository/WebElementLocators.java
+++ src/main/java/objectRepository/WebElementLocators.java
@@ -1,5 +1,11 @@
 package objectRepository;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class WebElementLocators {
 
+	public void searchButton(WebDriver driver, By by) {
+		
+	}
 }
